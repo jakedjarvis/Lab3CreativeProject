6423c62457064169b6e4db4e1664cdb6d7ebef65
jakedjarvis <30878986+jakedjarvis@users.noreply.github.com>
2017-10-19 12:35:57 -0600

    calcGPA function added

--- gpa.js
+++ gpa.js
@@ -14,17 +14,114 @@ function mainCtrl ($scope, classFactory) {
 	$scope.classes = classFactory.classes;
 
 	$scope.classes.push({
-		Grade: 'B',
-		Credits: 4
+		Grade: '',
+		Credits: '3'
 	});
 
 	console.log($scope.classes);
 
 	$scope.addClass = function(){
 		$scope.classes.push({
-			Grade: $scope.letter,
-			Credits: $scope.numCredits
+			Grade: '',
+			Credits: '3'
 		});
+		console.log($scope.classes);
+	};
+
+	$scope.calcGPA = function(){
+
+		var gpaConvertArray = [];
+		var creditsArray = [];
+
+		console.log($scope.classes[1]);
+
+		for (var i=0;i<$scope.classes.length;i++){
+
+			console.log("in the for loop");
+
+			if ($scope.classes[i].Grade == "A")
+			{
+				gpaConvertArray.push(4);
+			}
+			else if ($scope.classes[i].Grade == "A-")
+			{
+				gpaConvertArray.push(3.7);
+			}
+			else if ($scope.classes[i].Grade == "B+")
+			{
+				gpaConvertArray.push(3.3);
+			}
+			else if ($scope.classes[i].Grade == "B")
+			{
+				gpaConvertArray.push(3);
+			}
+			else if ($scope.classes[i].Grade == "B-")
+			{
+				gpaConvertArray.push(2.7);
+			}
+			else if ($scope.classes[i].Grade == "C+")
+			{
+				gpaConvertArray.push(2.3);
+			}
+			else if ($scope.classes[i].Grade == "C")
+			{
+				gpaConvertArray.push(2);
+			}
+			else if ($scope.classes[i].Grade == "C-")
+			{
+				gpaConvertArray.push(1.7);
+			}
+			else if ($scope.classes[i].Grade == "D+")
+			{
+				gpaConvertArray.push(1.3);
+			}
+			else if ($scope.classes[i].Grade == "D")
+			{
+				gpaConvertArray.push(1);
+			}
+			else if ($scope.classes[i].Grade == "D-")
+			{
+				gpaConvertArray.push(.7);
+			}
+			else if ($scope.classes[i].Grade == "F")
+			{
+				gpaConvertArray.push(0);
+			}
+			else if ($scope.classes[i].Grade == "")
+			{
+				gpaConvertArray.push(0);
+			}
+
+
+		}
+		console.log(gpaConvertArray);
+
+
+		for (var i=0;i<$scope.classes.length;i++){
+			creditsArray.push(parseInt($scope.classes[i].Credits));
+		}
+
+		console.log(creditsArray);
+
+		var total = 0;
+		var creditTotal = 0;
+
+		for(var i = 0; i < $scope.classes.length; i++){
+			total = total + (gpaConvertArray[i] * creditsArray[i])
+		}
+
+		creditsArray.forEach(function(element){
+			creditTotal = creditTotal + element;
+		});
+
+		console.log(total);
+		console.log(creditTotal);
+
+		var finalGPA = total/creditTotal;
+		$scope.finalGPA = finalGPA.toFixed(2);
+		console.log(finalGPA);
+
+
 	};
 
 
